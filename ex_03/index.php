4d1d403ef0051b3e14922d694e5e1a77f46f3db6
Nardone Clara <nardoneclara@MacBook-Air-de-Nardone.local>
2021-07-07 12:04:46 +0200

    exercice3

--- ex_03/index.php
+++ ex_03/index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-if (isset($_GET["name"]) != NULL){
+if (isset($_GET["name"])){
     $_SESSION["name"] = $_GET["name"];
 }
-if (isset($_SESSION["name"]) != NULL){
+if (isset($_SESSION["name"])){
     echo "Hello " . $_SESSION["name"];
 } else
 echo "Hello platypus"
